f773562d236360489bc04a86a5b96507a52c8a84
Matth0s <matheus.moreira.jpa@gmail.com>
2025-04-05 14:17:54 -0300

    feat: implementacao do validador primario dos parametros de monitoramento do programa

---
+++ MonitorArgs.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace StockQuoteAlert
+{
+	class MonitorArgs
+	{
+		public string Acao { get; private set; }
+		public double High { get; private set; }
+		public double Low { get; private set; }
+
+		public MonitorArgs(string[] args)
+		{
+			try
+			{
+				Acao = args[0];
+				double value1 = double.Parse(args[1]);
+				double value2 = double.Parse(args[2]);
+
+				if (value1 > value2)
+				{
+					High = value1;
+					Low = value2;
+				}
+				else
+				{
+					High = value2;
+					Low = value1;
+				}
+			}
+			catch (IndexOutOfRangeException)
+			{
+				throw new StockQuoteAlertException($"Numero de argumentos de monitoramento invalido, necessario 3");
+			}
+			catch (FormatException)
+			{
+				throw new StockQuoteAlertException($"O formato dos valores limite fornecidos para monitoramento é invalido");
+			}
+			catch (Exception ex)
+			{
+				throw new StockQuoteAlertException($"Ocorreu um com o processamento dos parametros passados para o programa: {ex.Message}");
+			}
+		}
+
+		public override string ToString()
+		{
+			return $"Acao: {Acao}\nHigh: {High}\nLow: {Low}";
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -6,14 +6,17 @@ namespace StockQuoteAlert
 	{
 		static void Main(string[] args)
 		{
+			MonitorArgs monitorArgs = new MonitorArgs(args);
+
 			ConfigSMTP configSMTP = new ConfigSMTP();
 
-			EmailSender emailSender = new EmailSender(configSMTP, "Ação");
+			// EmailSender emailSender = new EmailSender(configSMTP, "Ação");
 
-			emailSender.SendQuoteLow();
-			emailSender.SendQuoteHight();
-			emailSender.SendQuoteStable();
+			// emailSender.SendQuoteLow();
+			// emailSender.SendQuoteHight();
+			// emailSender.SendQuoteStable();
 
+			Console.WriteLine(monitorArgs);
 			Console.WriteLine(configSMTP);
 		}
 	}
